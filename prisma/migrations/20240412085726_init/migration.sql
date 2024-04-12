2f144088ea86f459b582e3b2c05fd315af1abf87
Zsiga Daniel <danielzsiga@gmail.com>
2024-04-12 13:27:42 +0300

    chore: configurations

---
+++ prisma/migrations/20240412085726_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" TEXT NOT NULL,
+    "vendor_name" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "due_date" TIMESTAMP(3) NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "paid" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,75 @@
+import { PrismaClient } from '@prisma/client';
+
+const invoices = [
+  {
+    vendor_name: 'Amazon',
+    amount: 100,
+    due_date: new Date('2024-01-01'),
+    description: 'Books, electronics, and other items',
+    paid: false,
+  },
+  {
+    vendor_name: 'Google',
+    amount: 200,
+    due_date: new Date('2024-02-01'),
+    description: 'Google Cloud services',
+    paid: true,
+  },
+  {
+    vendor_name: 'Microsoft',
+    amount: 300,
+    due_date: new Date('2024-03-01'),
+    description: 'Microsoft Office subscription',
+    paid: false,
+  },
+  {
+    vendor_name: 'Apple',
+    amount: 400,
+    due_date: new Date('2024-04-01'),
+    description: 'iPhone, iPad, and MacBook',
+    paid: true,
+  },
+  {
+    vendor_name: 'LinkedIn',
+    amount: 700,
+    due_date: new Date('2024-07-01'),
+    description: 'LinkedIn Ads',
+    paid: true,
+  },
+  {
+    vendor_name: 'Instagram',
+    amount: 1000,
+    due_date: new Date('2024-10-01'),
+    description: 'Instagram Ads',
+    paid: false,
+  },
+];
+
+const prisma = new PrismaClient();
+async function main() {
+  const john = await prisma.user.create({
+    data: {
+      email: 'john@mail.com',
+      name: 'John Doe',
+      password: 'password',
+    },
+  });
+
+  const createdInvoices = await prisma.invoice.createMany({
+    data: invoices.map((invoice) => ({
+      ...invoice,
+      userId: john.id,
+    })),
+  });
+
+  console.log({ john, createdInvoices });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
